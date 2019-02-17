b32503f98597d2363cc7a69364d1e7a6efbc4f50
Horácio Comé <horaciojr001@gmail.com>
2019-02-17 22:36:08 +0200

    new look to reading some more tweaks on item topic

--- app/src/main/java/io/github/horaciocome1/reaque/ui/posts/ReadFragment.kt
+++ app/src/main/java/io/github/horaciocome1/reaque/ui/posts/ReadFragment.kt
@@ -20,23 +20,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import io.github.horaciocome1.reaque.R
 import io.github.horaciocome1.reaque.data.posts.Post
-import io.github.horaciocome1.reaque.databinding.FragmentReadBinding
+import io.github.horaciocome1.reaque.databinding.FragmentRead2Binding
 import io.github.horaciocome1.reaque.ui.MainActivity
 import jp.wasabeef.glide.transformations.BlurTransformation
-import kotlinx.android.synthetic.main.fragment_read.*
+import kotlinx.android.synthetic.main.fragment_read_2.*
 
 class ReadFragment: Fragment() {
 
-    lateinit var binding: FragmentReadBinding
+    lateinit var binding: FragmentRead2Binding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentReadBinding.inflate(inflater, container, false)
+        binding = FragmentRead2Binding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -47,29 +49,38 @@ class ReadFragment: Fragment() {
             viewModel.getPosts(Post(postId)).observe(this, Observer { posts ->
                 when {
                     posts.isEmpty() -> {
-                        fragment_read_progressbar.visibility = View.VISIBLE
-                        fragment_read_content_scrollview.visibility = View.GONE
-                        fragment_read_cover_imageview.visibility = View.GONE
+//                        fragment_read_progressbar.visibility = View.VISIBLE
+//                        fragment_read_content_scrollview.visibility = View.GONE
+//                        fragment_read_cover_imageview.visibility = View.GONE
                     }
                     else -> {
-                        fragment_read_content_scrollview.visibility = View.VISIBLE
-                        fragment_read_cover_imageview.visibility = View.VISIBLE
-                        fragment_read_progressbar.visibility = View.GONE
+//                        fragment_read_content_scrollview.visibility = View.VISIBLE
+//                        fragment_read_cover_imageview.visibility = View.VISIBLE
+//                        fragment_read_progressbar.visibility = View.GONE
 
+                        val behavior = BottomSheetBehavior.from(fragment_read_bottom_sheet)
+                        behavior.run {
+                            state = BottomSheetBehavior.STATE_COLLAPSED
+//                            setBottomSheetCallback(BottomSheetCallback(
+//                                this@ReadFragment, fragment_read_cover2_imageview, posts[0].cover
+//                            ))
+                        }
                         posts[0].run {
                             binding.post = this
-                            (activity as MainActivity).supportActionBar?.title = title
+//                            (activity as MainActivity).supportActionBar?.title = title
                             Glide.with(this@ReadFragment).run {
-                                load(cover).into(fragment_read_cover_imageview)
-                                load(cover).apply(RequestOptions.bitmapTransform(BlurTransformation(2, 14)))
+                                load(cover)
+                                    .into(fragment_read_cover_imageview)
+                                load(cover)
+                                    .apply(RequestOptions.bitmapTransform(BlurTransformation(2, 14)))
                                     .into(fragment_read_cover2_imageview)
-                                load(user.pic).apply(RequestOptions.circleCropTransform())
-                                    .into(fragment_read_profile_pic_imageview)
+//                                load(user.pic).apply(RequestOptions.circleCropTransform())
+//                                    .into(fragment_read_profile_pic_imageview)
                             }
-                            fragment_read_profile_pic_imageview.setOnClickListener {
-                                val openImage = ReadFragmentDirections.actionOpenImage(user.pic)
-                                Navigation.findNavController(it).navigate(openImage)
-                            }
+//                            fragment_read_profile_pic_imageview.setOnClickListener {
+//                                val openImage = ReadFragmentDirections.actionOpenImage(user.pic)
+//                                Navigation.findNavController(it).navigate(openImage)
+//                            }
                         }
                     }
                 }
@@ -81,6 +92,28 @@ class ReadFragment: Fragment() {
         super.onResume()
         if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
             (activity as MainActivity).supportActionBar?.show()
+    }
+
+    class BottomSheetCallback(
+        private val fragment: Fragment,
+        private val cover: ImageView,
+        private val url: String
+    ) : BottomSheetBehavior.BottomSheetCallback() {
+
+        override fun onSlide(p0: View, p1: Float) {
+        }
+
+        override fun onStateChanged(p0: View, p1: Int) {
+            Glide.with(fragment)
+                .load(
+                    if (p1 == BottomSheetBehavior.STATE_EXPANDED)
+                        R.color.primaryColor
+                    else
+                        url
+                )
+                .into(cover)
+        }
+
     }
 
 }
