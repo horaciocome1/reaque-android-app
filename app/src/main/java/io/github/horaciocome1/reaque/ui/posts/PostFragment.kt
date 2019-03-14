e5ba2f72fc6ae3221653762b3239162979154c15
Horácio Comé <horaciojr001@gmail.com>
2019-03-14 18:16:43 +0200

    fixes on post fragment [action bar not showing]

--- app/src/main/java/io/github/horaciocome1/reaque/ui/posts/PostFragment.kt
+++ app/src/main/java/io/github/horaciocome1/reaque/ui/posts/PostFragment.kt
@@ -15,12 +15,14 @@
 
 package io.github.horaciocome1.reaque.ui.posts
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import io.github.horaciocome1.reaque.databinding.FragmentPostBinding
+import io.github.horaciocome1.reaque.ui.MainActivity
 
 class PostFragment : Fragment() {
 
@@ -29,6 +31,12 @@ class PostFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentPostBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
+            (activity as MainActivity).supportActionBar?.show()
     }
 
 }
