431f690aad46e44fb0dd942b977ca1130f819688
Horácio Comé <horaciojr001@gmail.com>
2019-04-11 19:47:36 +0200

    sign out 2

--- app/src/main/java/io/github/horaciocome1/reaque/ui/settings/SettingsFragment.kt
+++ app/src/main/java/io/github/horaciocome1/reaque/ui/settings/SettingsFragment.kt
@@ -16,17 +16,14 @@
 package io.github.horaciocome1.reaque.ui.settings
 
 import android.content.Intent
-import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
 import io.github.horaciocome1.reaque.R
 import io.github.horaciocome1.reaque.ui.MainActivity
 import io.github.horaciocome1.reaque.utilities.isPortrait
