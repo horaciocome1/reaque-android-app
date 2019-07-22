e39ac24b0e0aa72236f3addc0c4689a6207867b1
Horácio Comé <horaciojr001@gmail.com>
2019-07-22 13:43:34 +0200

    complete readings phase

--- app/src/main/java/io/github/horaciocome1/reaque/data/readings/ReadingsService.kt
+++ app/src/main/java/io/github/horaciocome1/reaque/data/readings/ReadingsService.kt
@@ -1,6 +1,7 @@
 package io.github.horaciocome1.reaque.data.readings
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import io.github.horaciocome1.reaque.data.posts.Post
 import io.github.horaciocome1.reaque.util.addSimpleAuthStateListener
@@ -12,16 +13,24 @@ class ReadingsService : ReadingsInterface {
 
     private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
+    private val increment: Map<String, FieldValue> by lazy {
+        val increment = FieldValue.increment(1)
+        mapOf("readings" to increment)
+    }
+
     override fun read(post: Post, onSuccessListener: (Unit?) -> Unit) {
         auth.addSimpleAuthStateListener { user ->
-            db.runTransaction {
-                val ref = db.document("posts/${post.id}/readings/${user.uid}")
-                val snapshot = it[ref]
-                if (snapshot["name"] == null) {
-                    val map = user.map.plus("post" to mapOf("id" to post.id))
-                    it.set(ref, map)
+            val readingRef = db.document("posts/${user.uid}/readings/${post.id}")
+            val postRef = db.document("posts/${post.id}")
+            readingRef.get().addOnSuccessListener { snapshot ->
+                if (snapshot["timestamp"] == null) {
+                    val data = user.map.plus("post" to mapOf("id" to post.id))
+                    db.runBatch {
+                        it.set(readingRef, data)
+                        it.set(postRef, increment)
+                    }
                 }
-            }.addOnSuccessListener(onSuccessListener)
+            }
         }
     }
 
